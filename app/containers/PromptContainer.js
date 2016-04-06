17482def867d600a94f3ee1466d5988cf356a301
Iting <wenit123@gmail.com>
2016-04-06 16:25:03 -0700

    update to 20.46 5

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,7 +1,11 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+// seperate UI to different file, to seperate the logic
 var Prompt = require('../components/Prompt');
 
+
 var PromptContainer = React.createClass({
+  // dis routing, proptype ????
   contextTypes: {
     router: React.PropTypes.object.isRequired
   },
@@ -16,17 +20,20 @@ var PromptContainer = React.createClass({
     this.setState({
       username: ''
     });
-
+    // go battle 
     if (this.props.routeParams.playerOne) {
       var pathname =
       this.context.router.push({
+        // push object
         pathname: '/battle',
         query: {
           playerOne: this.props.routeParams.playerOne,
           playerTwo: this.state.username,
         }
       })
+    // go player 2
     } else {
+      //push to the path 
       this.context.router.push('/playerTwo/' + this.state.username)
     }
   },
@@ -37,6 +44,7 @@ var PromptContainer = React.createClass({
   },
   render: function () {
     return (
+      //the stuff that you want to pass
       <Prompt
         onSubmitUser={this.handleSubmitUser}
         onUpdateUser={this.handleUpdateUser}
